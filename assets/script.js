1e602a9cf9384a0c000cfe0b03a23135237a2cf7
Sheryl L <s_lawrence0@yahoo.ca>
2024-05-30 13:31:28 -0400

    Code added to script.js to fetch weather from OpenWeather

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,23 @@
 document.getElementById('searchForACity').addEventListener('click', function() {
-    var city = document.getElementByIf('searchForACity').value;
-    var apiKey = ;
-    var apiUrl = ;
+    var city = document.getElementById('searchForACity').value;
+    var apiKey = '90144234923256377c0e16ccc86b8d67';
+    var apiUrl = `http://api.openweathermap.org/data2.5/onecall?`;
 
     fetch(apiUrl).then(response => response.json())
     
     .then(data => {
         if (data.code === 200) {
-            var weatherResult = `
-            <`
+            var weatherResults = `
+            <h2>${data.name}<h2>
+            <p>Temperature: ${data.main.temp} °C</p>
+            `;
+            document.getElementById('fiveDays').innerHTML = weatherResults;
+        } else {
+            document.getElementById('fiveDays').innerHTML = '<p>This city could not be found</p>';
         }
     })
-})
+    .catch(error => {
+        console.error('There was an error fetching this weather data', error);
+        document.getElementById('fiveDays').innerHTML = '<p>There was an error fetching this weather data</p>';
+    });
+});
