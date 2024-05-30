13e8bf5fe992d082cd9b19c520687e20120d8e91
Sheryl L <s_lawrence0@yahoo.ca>
2024-05-30 12:53:24 -0400

    Code added to script.js - event listener and variables created

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+document.getElementById('searchForACity').addEventListener('click', function() {
+    var city = document.getElementByIf('searchForACity').value;
+    var apiKey = ;
+    var apiUrl = ;
+
+    fetch(apiUrl).then(response => response.json())
+    
+    .then(data => {
+        if (data.code === 200) {
+            var weatherResult = `
+            <`
+        }
+    })
+})
