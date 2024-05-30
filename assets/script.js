3d5bbc282bc0e5a8353246456d267023e3448cb8
Sheryl L <s_lawrence0@yahoo.ca>
2024-05-30 16:50:57 -0400

    Additional comments added

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
+// Event listener added to 'Search' button
 document.getElementById('searchButton').addEventListener('click', function() {
+    // Variables created 
     var city = document.getElementById('cityInput').value;
-    var apiKey = '90144234923256377c0e16ccc86b8d67';
-    var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=90144234923256377c0e16ccc86b8d67`;
+    var apiKey = '90144234923256377c0e16ccc86b8d67'; // API key 
+    var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=90144234923256377c0e16ccc86b8d67`; // API URL
 
+    // Fetch initiates request to the URL and parses it as JSON
     fetch(apiUrl).then(response => response.json())
-    
+    // Handle parsed data and if successful update the <p> tag with the weather results  
     .then(data => {
         if (data.cod === 200) {
             var weatherResults = `
@@ -14,9 +17,11 @@ document.getElementById('searchButton').addEventListener('click', function() {
             `;
             document.getElementById('fiveDays').innerHTML = weatherResults;
         } else {
+            // Handle errors with custom message and render to page in <p> tag
             document.getElementById('fiveDays').innerHTML = '<p>This city could not be found</p>';
         }
     })
+
     .catch(error => {
         console.error('There was an error fetching this weather data', error);
         document.getElementById('fiveDays').innerHTML = '<p>There was an error fetching this weather data</p>';
